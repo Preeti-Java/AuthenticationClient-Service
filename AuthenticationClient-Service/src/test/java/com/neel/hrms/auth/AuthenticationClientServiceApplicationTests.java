3b8126961519f00c5c76c5c282282b65dfdd0609
Preeti-Java <in.preetidhiman@gmail.com>
2023-12-06 17:40:13 +0530

    first commit

---
+++ AuthenticationClient-Service/src/test/java/com/neel/hrms/auth/AuthenticationClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neel.hrms.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
